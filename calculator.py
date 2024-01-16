0e54652680876629bcaa6aef7b50525022c917a2
Karan Singh Bisht <thakurkaran6416@gmail.com>
2024-01-16 17:31:15 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+first = input("entr first number: ")
+second = input("enter second operator: ")
+Operator = input("enter operator: ")
+
+if Operator == "+":
+    print(int(first) + int(second))
+
+elif Operator == "-":
+    print(int(first) + int(second))
+
+elif Operator == "*":
+    print(int(first) + int(second))
+
+elif Operator == "/":
+    print(int(first) + int(second))
+
+elif Operator == "%":
+    print(int(first) + int(second))
+
+else :
+    print("Invalid Input")
